97bdafb10040af7324b9d940e531546400aa0ee1
Somfe <somfev@gmail.com>
2022-09-12 06:27:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 /**
- * main -Entry point
+ * main - ENtry point
  *
- * Return: Always 1 (Sucess)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char str1[]="and that piece of art is useful\" - Dora korpar, 2015-10-19";
+	char str1{} = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
 
 	write(2, str1, 59);
 	return (1);

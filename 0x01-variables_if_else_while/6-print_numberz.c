bb435172341542766031994230515451a2b01d31
Sommy <57346204+sommyvick@users.noreply.github.com>
2022-09-12 22:51:11 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,18 @@
-#fjffjjfj
-fjfjfjf
+#include <stdio.h>
 
-fkfkf
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

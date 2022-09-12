6bfc02105d608893d210da8ea5247df49fd806b3
Somfe <somfev@gmail.com>
2022-09-12 06:46:14 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
-
 /**
- * main - print exactly "and that piece of art is useful"
- * - Dora korpar, 2012-10-19,
- * followed by a new, to the standard error,
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always ! (Success)
  */
 int main(void)
 {
-	char str1{}="and that piece of art is useful\" - Dora korpar, 2015-10-19";
+	char str1{} = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str1, 59);
 	return (1);

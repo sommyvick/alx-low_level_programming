97ecc4300351fcfd22519f2d7dc9d86c43550d07
Somfe <somfev@gmail.com>
2022-09-12 07:41:06 -0700

    Postivie or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -19,6 +20,10 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }

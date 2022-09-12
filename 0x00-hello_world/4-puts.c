74cc4b790ebf4e6d98b8ff06f5947419c760a278
Somfe <somfev@gmail.com>
2022-09-12 05:10:28 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/*
- *main - Entry point
- *Return: always 0 (sucess)
+/**
+ * main - Entry point
+ * Return: always 0 (sucess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
- 

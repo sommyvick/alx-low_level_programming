704051d699e858e5e66664c28513c0d2cd76a382
Sommy <57346204+sommyvick@users.noreply.github.com>
2022-09-15 22:53:24 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _isupper - checks for uppercase characters
+ * @c: c -- variable
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
